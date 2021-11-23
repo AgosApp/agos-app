cde67e819f84874efbdcefc893e4bad5ca66bcdf
mboufelja <mboufelja@moenn.tech>
2021-11-23 19:14:10 +0100

    initiate REST API

---
+++ agos-api/src/test/java/UFC/Agos/AgosApplicationTests.java
@@ -0,0 +1,13 @@
+package UFC.Agos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
