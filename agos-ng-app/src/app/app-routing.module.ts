da11d4405247dc014d01d1608456c02a925e6fe2
Youssef Jounaid <jounaid.ysf@gmail.com>
2021-12-02 13:45:39 +0100

    feat(routing): configured navbar routing

--- agos-ng-app/src/app/app-routing.module.ts
+++ agos-ng-app/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClassroomsComponent } from './classrooms/classrooms.component';
+import { NotationComponent } from './notation/notation.component';
+import { ProfessorsComponent } from './professors/professors.component';
+import { SessionsComponent } from './sessions/sessions.component';
+import { StudentsComponent } from './students/students.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'classrooms', component: ClassroomsComponent},
+  {path:'notation', component: NotationComponent},
+  {path:'sessions', component: SessionsComponent},
+  {path:'students', component: StudentsComponent},
+  {path:'professors', component: ProfessorsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
