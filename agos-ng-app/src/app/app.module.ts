bf7c06847473d107f8dc03d3aa6ab9e95b7eaf1e
Youssef Jounaid <jounaid.ysf@gmail.com>
2021-12-02 13:28:29 +0100

    feat(app): generated static views

--- agos-ng-app/src/app/app.module.ts
+++ agos-ng-app/src/app/app.module.ts
@@ -6,11 +6,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ClassroomsComponent } from './classrooms/classrooms.component';
+import { NotationComponent } from './notation/notation.component';
+import { SessionsComponent } from './sessions/sessions.component';
+import { StudentsComponent } from './students/students.component';
+import { ProfessorsComponent } from './professors/professors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ClassroomsComponent,
+    NotationComponent,
+    SessionsComponent,
+    StudentsComponent,
+    ProfessorsComponent
   ],
   imports: [
     BrowserModule,
