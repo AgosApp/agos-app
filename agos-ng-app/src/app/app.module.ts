b0bfafdcd834aa90edcfd0dc65647b2ce79132bd
Youssef Jounaid <jounaid.ysf@gmail.com>
2021-12-10 01:26:55 +0100

    feat(layout): added outlet margins, grid for cards in notation component

--- agos-ng-app/src/app/app.module.ts
+++ agos-ng-app/src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatMenuModule} from '@angular/material/menu';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -29,7 +34,12 @@ import { FooterComponent } from './components/footer/footer.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatCardModule,
+    MatGridListModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- agos-ng-app/src/app/components/notation/notation.component.ts
+++ agos-ng-app/src/app/components/notation/notation.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-notation',
   templateUrl: './notation.component.html',
   styleUrls: ['./notation.component.sass']
 })
 export class NotationComponent implements OnInit {
+
+  cards = [  
+    { title: 'Critères', cols: 1, rows: 1 },  
+    { title: 'Créer Groupe de Notation', cols: 1, rows: 2 },  
+    { title: 'Groupes de Notation', cols: 1, rows: 1 }
+  ]; 
 
   constructor() { }
 
