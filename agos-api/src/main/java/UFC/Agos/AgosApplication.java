a53903e3c28a6706d2cb9fbb4ebf611a7984002e
Mourtada-Bf <mourtadaboufelja123@gmail.com>
2022-01-02 16:45:31 +0100

    feat(api): add Corse Configuration

--- agos-api/src/main/java/UFC/Agos/AgosApplication.java
+++ agos-api/src/main/java/UFC/Agos/AgosApplication.java
@@ -16,6 +16,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -23,7 +24,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableSwagger2
 public class AgosApplication {
 
